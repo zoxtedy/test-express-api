ecc38a0aeffa3c84b290f4854b652d965d60a8ac
zoxtedy <109044073+zoxtedy@users.noreply.github.com>
2022-07-11 03:40:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use("/people", usersRoutes);
+app.use("/users", usersRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 app.all("*", (req, res) =>res.status(404).send("You've tried reaching a route that doesn't exist."));
 
