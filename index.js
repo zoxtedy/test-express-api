5b7014b1efd8cb29c55f524e3b0260625233a90c
zoxtedy <109044073+zoxtedy@users.noreply.github.com>
2022-07-18 17:11:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const { wakeDyno, wakeDynos } = require('heroku-keep-awake');
+
 import usersRoutes from "./routes/users.js";
 
-const app = express();
 const PORT = 5000;
+const DYNO_URL = 'https://my-app.herokuapp.com';
+
+
+const app = express();
+
 
 app.use(bodyParser.json());
+const DYNO_URL = 'https://neon-api-test-task.herokuapp.com'
+
+const opts = {
+    interval: 2,
+    logging: true,
+    stopTimes: { start: '08:00', end: '20:00' }
+}
 
 app.use("/users", usersRoutes);
 app.get("/", (req, res) => res.send("<!DOCTYPE html><html><body><h1>Welcome to the Users API!</h1><h2>Endpoints:</h2><ul><li>GET /users<li>POST /users<ul><li>name: string<li>dob: date<li>role: \"SECURITY\",\"MANAGER\",\"WORKER\"<li>active: boolean</ul><li>GET /users/:id<li>PATCH /users/:id<ul><li>role: \"SECURITY\",\"MANAGER\",\"WORKER\"<li>active: boolean</ul><li>DELETE /users/:id</body></html>"));
@@ -14,4 +27,6 @@ app.all("*", (req, res) =>res.status(404).send("You've tried reaching a route th
 
 //.listen(process.env.PORT || 5000)
 
-app.listen(process.env.PORT || 5000); // Adapted for heroku. Old line kept in this comment >>listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    wakeDyno(DYNO_URL,opts); 
+})
