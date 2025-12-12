82785fa99aa802470a6fe34b7f5938c8aa5adf05
Zoran Gacesa <zoran.gacesa@neon-free.ch>
2025-12-12 09:53:46 +0100

    updated HTML to reflect the cahnges in the API

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(bodyParser.json());
 app.use("/", authRoutes);
 app.use("/users", usersRoutes);
 app.use("/admin", adminRoutes);
-app.get("/", (req, res) => res.send("<!DOCTYPE html><html><body><h1>Welcome to the Users API!</h1><h2>Endpoints:</h2><ul><li>GET /users<li>POST /users<ul><li>name: string<li>dob: date<li>role: \"SECURITY\",\"MANAGER\",\"WORKER\"<li>active: boolean</ul><li>GET /users/:id<li>PATCH /users/:id<ul><li>role: \"SECURITY\",\"MANAGER\",\"WORKER\"<li>active: boolean</ul><li>DELETE /users/:id</body></html>"));
+app.get("/", (req, res) =>
+    res.send(
+        "<!DOCTYPE html><html><body><h1>Welcome to the Users API!</h1><h2>Endpoints:</h2><ul><li>GET /authenticate?user=&lt;username&gt;&password=&lt;password&gt; (returns Bearer token required for /users)</li><li>GET /users</li><li>POST /users<ul><li>name: string</li><li>dob: date</li><li>role: \"SECURITY\",\"MANAGER\",\"WORKER\"</li><li>active: boolean</li></ul></li><li>GET /users/:id</li><li>PATCH /users/:id<ul><li>role: \"SECURITY\",\"MANAGER\",\"WORKER\"</li><li>active: boolean</li></ul></li><li>DELETE /users/:id</li></ul></body></html>"
+    )
+);
 app.all("*", (req, res) =>res.status(404).send("You've tried reaching a route that doesn't exist."));
 
 //.listen(process.env.PORT || 5000)
